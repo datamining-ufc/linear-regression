e5fae0dcfbbe8596856f5b244f969b84406eb5fd
Manoel Vilela <manoel_vilela@engineer.com>
2018-07-10 11:12:32 -0300

    Ignore h value from ttest on p_value function

--- src/p_value.m
+++ src/p_value.m
@@ -2,5 +2,5 @@ function p = p_value(x, y)
 % P_VALUE calculate the p-value between the vectors X and Y using
 % the built-in ttest (Test of T-distribution).
 % REF: https://www.mathworks.com/help/stats/ttest.html
-    [h, p] = ttest(x, y)
+    [~, p] = ttest(x, y);
 end
