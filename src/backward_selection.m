78f0711b3b16341ded38554c16fba687618cd6ea
Manoel Vilela <manoel_vilela@engineer.com>
2018-07-10 11:49:09 -0300

    Implement backward_selection

--- src/backward_selection.m
+++ src/backward_selection.m
@@ -1,4 +1,4 @@
-function vars = backward_selection(X, y)
+function vars = backward_selection(T, y)
 % BACKWARD_SELECTION implements a algorithm for variable
 % selection to the task of multiple linear regression in the
 % following:
@@ -18,4 +18,34 @@ function vars = backward_selection(X, y)
 %     X: a table in M x N form, where M, N > 1
 %     Y: a column-vector with N elements
 %     VARS: table of variables by column choosed
+
+    p_value_threshold = 0.05;
+    stack_vars = T.Properties.VariableNames(:); % vector of variable names
+    
+    while max_p_value(T{:, stack_vars}, y) > p_value_threshold
+       n = length(stack_vars);
+       worst_var = 1;
+       p_value_max = -inf;
+       for i = 1:n
+           var_name = stack_vars(i);
+           p = p_value(T{:, var_name}, y);
+           if p > p_value_max
+              p_value_max = p;
+              worst_var = i;
+           end
+       end
+       
+       stack_vars(worst_var) = []; % remove worst_var
+    end
+    
+    vars = T(:, stack_vars);
+end
+
+function p = max_p_value(X, y)
+    [~, n] = size(X);
+    p_values = ones(n);
+    for i = 1:n
+        p_values = p_value(X(:, i), y);
+    end
+    p = max(p_values);
 end
